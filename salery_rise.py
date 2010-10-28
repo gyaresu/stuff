e16326bfbf24c2f252028567bd77cebefc6a5c9f
Gareth <me@gyaresu.org>
2010-10-28 15:54:13 +1000

    a little salery script for determining future earnings with increase

---
+++ salery_rise.py
@@ -0,0 +1,14 @@
+from decimal import *
+
+def increase(x = input('What salery?: ')):	
+	y = 2010
+	q = x
+	for i in range(21):
+		print y, "\t", "%.2f" % x, "\t",
+		print "%.2f" % q
+		x = x * 1.02
+		q = q + x
+		y = y + 1
+
+increase()
+	
